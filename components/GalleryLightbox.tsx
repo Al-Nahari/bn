6ce3c8ebd74e5685862380f1e5c9fe0ad434af4d
Al-Nahari <nahari771735416@gmail.com>
2026-03-30 22:16:27 +0300

    do colors

--- components/GalleryLightbox.tsx
+++ components/GalleryLightbox.tsx
@@ -47,7 +47,7 @@ export default function GalleryLightbox({ images, title }: GalleryLightboxProps)
    return (
      <>
        {/* Gallery Grid */}
-       <div className="grid md:grid-cols-2 gap-8">
+       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
          {images.map((item, index) => (
            <div
              key={index}
